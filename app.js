45d4c3a9dd9ca034f5dac9dce4096aad72174943
Alex <aguo11@my.bcit.ca>
2020-12-02 01:45:01 -0800

    app.js updated, now working: -'save' button now adds list item to side bar with title of JS object added to array. (adding textarea input to notesArray was in previous build) Still needs doing: -clickable note title list that returns text value from its corresponding object 'body' in the array and displays it in the text area –dark mode

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let notesArray = [
-    {title:"", body:""},
+const notesArray = [
+    {title: "", body: ""}
 ]
 
 const cancel = document.getElementById('cancel');
@@ -7,7 +7,6 @@ const note = document.getElementsByTagName('note');
 const new_note = document.getElementById('new_note');
 const save = document.getElementById('save');
 var text = document.getElementById('textbox')
-var ul = 
 
 
 cancel.addEventListener("click", function() {
@@ -26,6 +25,26 @@ new_note.addEventListener("click", function() {
 save.addEventListener("click", function() {
     var text = document.getElementById('textbox').value.split('\n');
     notesArray.unshift({title: text[0], body: text[1]})
-    notesArray.length = 10
-    console.log(notesArray)
+    notesArray.pop()
+    createNoteList(notesArray)
+    createNoteContent(notesArray)
 })
+
+function createNoteList(note_list) {
+    const notes = document.querySelector('#notes_list')
+    notes.innerHTML = ""
+    const noteName = `
+        <ul>
+            ${note_list.map(i => `<li>${i.title}</li>`).join('\n')}
+        </ul>
+    `
+    notes.insertAdjacentHTML('afterbegin', noteName)
+}
+
+function createNoteContent(note_list) {
+    const noteText = document.querySelector('#textbox').value
+    const formattedNoteList = noteText.replace(/\n/, '|||').split('|||')
+    note_list.push({...formattedNoteList})
+    console.log(note_list)
+}
+
